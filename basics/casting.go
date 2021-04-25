b0eef3f1c6aa9a62298af83bbd85dd523d60c3f0
faultyagatha <valeria.barvinska@gmail.com>
2021-04-25 18:27:16 +0200

    add size of and pointer extended example

--- basics/casting.go
+++ basics/casting.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
 
 func printBytes(n int) {
 	fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
+}
+
+func main() {
+	s := make([]int32, 1000)
+	var i32 int32
+	var i16 int32
+	var f32 float32
+	var f64 float64
+	var r rune
+	var str string
+	var p *int
+
+	fmt.Println("Size of int16:", unsafe.Sizeof(i16))
+	fmt.Println("Size of int32:", unsafe.Sizeof(i32))
+	fmt.Println("Size of float32:", unsafe.Sizeof(f32))
+	fmt.Println("Size of float64:", unsafe.Sizeof(f64))
+	fmt.Println("Size of rune:", unsafe.Sizeof(r))
+	fmt.Println("Size of str:", unsafe.Sizeof(str))
+	fmt.Println("Size of p:", unsafe.Sizeof(p))
+
+	fmt.Println("Size of []int32:", unsafe.Sizeof(s))
+	fmt.Println("Size of [1000]int32:", unsafe.Sizeof([1000]int32{}))
+	fmt.Println("Real size of s:", unsafe.Sizeof(s)+unsafe.Sizeof([1000]int32{}))
 }
--- basics/pointers-deallocation.go
+++ basics/pointers-deallocation.go
@@ -10,7 +10,16 @@ func foo() *int {
 	return &x //returns a reference, address
 }
 
-//POINTER RECEIVERS
+func addOneByCopy(x int) {
+	x += 1
+}
+
+func addOne(x *int) {
+	//dereference the pointer before adding
+	*x += 1
+}
+
+//******** POINTER RECEIVERS
 //we can declare methods with pointer receivers
 //these methods can modify the value to which the receiver points
 type Vertex struct {
@@ -30,12 +39,14 @@ func (v *Vertex) Scale(f float64) {
 
 //below we use a pointer in a usual manner
 func Scale(v *Vertex, f float64) {
+	//go implicitly dereferences the pointer
+	//in C: v->v.X
 	v.X = v.X * f
 	v.Y = v.Y * f
 }
 
 func main() {
-	var y *int
+	var y *int           //pointer to an int
 	y = foo()            //de-references the address and gets the val from it
 	fmt.Printf("%d", *y) //garbage collector will dealloc the memory for this function in Go
 	v := Vertex{3, 4}
@@ -44,4 +55,12 @@ func main() {
 	fmt.Println((v.Abs())) //50
 	Scale(&v, 10)
 	fmt.Println((v.Abs())) //500
+
+	x := 2
+	var xPtr *int = &x
+
+	addOneByCopy(x)
+	fmt.Println("x after addOneByCopy: ", x) //2
+	addOne(xPtr)
+	fmt.Println("x after addOne: ", x) //3
 }
