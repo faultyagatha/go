95317bd2f28b27211509160c4f998df9450c517b
Valeria <valeria.barvinska@gmail.com>
2023-08-28 08:57:49 +0200

    add race condition example

--- concurrency/scheduler.go
+++ concurrency/scheduler.go
@@ -12,32 +12,36 @@ func printAlphabet(isLower bool, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	if isLower {
-		for i := 0; i < 10000; i++ {
+		for i := 0; i < 10; i++ {
 			for char := 'a'; char < 'a'+26; char++ {
 				fmt.Printf("%c ", char)
 			}
 		}
 	} else {
-		fmt.Println("\nStart Shorter Routine.\n")
+		fmt.Println("\n--- Start Shorter Routine.--- \n")
 		for i := 0; i < 3; i++ {
 			for char := 'A'; char < 'A'+26; char++ {
 				fmt.Printf("%c ", char)
 			}
 		}
-		fmt.Println("\nEnd Shorter Routine.\n")
+		fmt.Println("\n--- End Shorter Routine. ---\n")
 	}
 }
 
 func main() {
 	// Allocate 1 logical processor for the scheduler to use
-	runtime.GOMAXPROCS(1)
+	// runtime.GOMAXPROCS(1)
+
+	// Allocate 2 logical processors for the scheduler to use
+	// now 2 tasks will be running in parallel
+	runtime.GOMAXPROCS(2)
 
 	// main need to wait for the goroutines to finish
 	var wg sync.WaitGroup
-	// add 2 goroutines to the wg
+	// Add 2 goroutines to the wg
 	wg.Add(2)
 
-	// these two goroutines will run concurrently
+	// These two goroutines will run concurrently
 	// and will be swapped from time to time
 	// (see the output)
 	go printAlphabet(false, &wg)
